ba2f1e88959fb2e2a546b69b58066295dbd9a18c
halosky-Wendy <626090763@qq.com>
2022-09-08 20:21:23 +0800

    UART of Define

--- src/kernel/memlayout.h
+++ src/kernel/memlayout.h
@@ -6,7 +6,14 @@
 #define UART_THR 0
 #define LSR 5
 #define THR 0
- 
+#define UART_THR (volatile uint64 *)(UART + 0x00) // THR:transmitter holding register
+#define UART_RHR (volatile uint64 *)(UART + 0x00) // RHR:Receive holding register
+#define UART_DLL (volatile uint64 *)(UART + 0x00) // LSB of Divisor Latch (write mode)
+#define UART_DLM (volatile uint64 *)(UART + 0x01) // MSB of Divisor Latch (write mode)
+#define UART_IER (volatile uint64 *)(UART + 0x01) // Interrupt Enable Register
+#define UART_LCR (volatile uint64 *)(UART + 0x03) // Line Control Register
+#define UART_LSR (volatile uint64 *)(UART + 0x05) // LSR:line status register
+#define UART_LSR_EMPTY_MASK 0x40                   // LSR Bit 6: Transmitter empty; both the THR and LSR are empty
 
 #define ReadReg(reg) (*(Reg(reg)))
 #define WriteReg(reg,v) (*(Reg(reg)) = v)
